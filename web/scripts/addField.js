966360992feee946ac6b99c8871f727587579912
Vinicius Santos <vinicius.santosama@gmail.com>
2020-08-06 19:06:19 -0300

    day3.1

--- web/scripts/addField.js
+++ web/scripts/addField.js
@@ -1,14 +1,16 @@
-// Procurar o botão
 document.querySelector("#add-time").addEventListener('click', cloneField)
-// Quando clicar no botão
 
 
-//Executar uma ação
 function cloneField() {
-    //Duplicar os campos
-    console.log("Cheguei aqui")
+    
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
 
-    // Colocar na página
+    const fields = newFieldContainer.querySelectorAll('input')
+    
+    fields.forEach(function(field) {
+        field.value = ""
+    }) 
 
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
 }
         
