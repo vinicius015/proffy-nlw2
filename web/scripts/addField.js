cb2f645b45de206386b7110c3935b96f1b3ed3bc
Vinicius Santos <vinicius.santosama@gmail.com>
2020-08-06 00:09:08 -0300

    day 3

---
+++ web/scripts/addField.js
@@ -0,0 +1,14 @@
+// Procurar o botão
+document.querySelector("#add-time").addEventListener('click', cloneField)
+// Quando clicar no botão
+
+
+//Executar uma ação
+function cloneField() {
+    //Duplicar os campos
+    console.log("Cheguei aqui")
+
+    // Colocar na página
+
+}
+        
