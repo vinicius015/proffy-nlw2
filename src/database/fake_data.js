e949ab5e4dcff23fdac3beb7de8006444207aa7b
Vinicius Santos <vinicius.santosama@gmail.com>
2020-08-08 17:31:33 -0300

    day 5

---
+++ src/database/fake_data.js
@@ -0,0 +1,52 @@
+//Dados
+const proffys = [
+    {
+        name: "Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "896456456", 
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: 20, 
+        weekday: [0], 
+        time_from: [720], 
+        time_to: [1220] 
+    },
+
+    {
+        name: "Daniele Evangelista", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "896456456", 
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: 20, 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220] 
+    },
+
+    {
+        name: "Vinicius dos Santos Amaral", 
+        avatar: "https://avatars2.githubusercontent.com/u/61382361?s=460&u=96e7cdbaaf317dec285fc40853071f49030e006c&v=4", 
+        whatsapp: "896456456", 
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: 20, 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220] 
+    },
+
+    {
+        name: "Glauce Aparecida dos Santos", 
+        avatar: "https://media-exp1.licdn.com/dms/image/C4E03AQE6tsDjiz8LDQ/profile-displayphoto-shrink_200_200/0?e=1602115200&v=beta&t=-gwgwddqj8YuWG2tgh0Jvxf_GtCfDItdlblMRXf8KjY", 
+        whatsapp: "896456456", 
+        bio: "Sou linda, maravilhosa, cheirosa, uma mãe incrível, e nota 10000000000", 
+        subject: "Ser Linda", 
+        cost: 100, 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220] 
+    }
+] 
+
+
